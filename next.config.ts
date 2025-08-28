1660d92712602370cb6af24c566715817a0d2964
Rolando Barberi <rolandobarberi@gmail.com>
2025-08-28 22:59:06 +0000

    aun no aparecen, revisa la estrucutra que he creado y algún posible erro

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
