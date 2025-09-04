988de521f6d5eb19f1da667499be2c456e4d8b24
Rolando Barberi <rolandobarberi@gmail.com>
2025-09-04 22:01:50 +0000

    perfecto ya funciona, debemos corregir varias cosas: 1- en el footer, ca

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Bot className="h-6 w-6 text-primary" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            © {new Date().getFullYear()} FinanceFlow AI. Todos los derechos reservados.
+            © {new Date().getFullYear()} BotPager. Todos los derechos reservados.
           </p>
         </div>
         <div className="flex items-center gap-4">
