3d81bd605ffe951fbb81c2caa9beb4532d68d9c9
Rolando Barberi <rolandobarberi@gmail.com>
2025-09-04 16:32:42 +0000

    con la actual configuración puedo hacer el build para llevarme el sitio

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,8 +2,10 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
